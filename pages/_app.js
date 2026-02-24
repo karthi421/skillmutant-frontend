f1535ea0c33aa2b15aada36fea29487a3ea32c86
karthik <kandellikarthik7@gmail.com>
2026-02-24 10:22:30 +0530

    app updating

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,21 @@ import "../styles/globals.css";
 
 import { SessionProvider, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-
+import { useEffect } from "react";
 import ChatWidget from "../components/chat/ChatWidget";
 
 function AppContent({ Component, pageProps }) {
   const { status } = useSession();
   const router = useRouter();
-
   const path = router.asPath;
+    // 🔥 ADD THIS WAKEUP EFFECT
+  useEffect(() => {
+    fetch("https://skillmutant-backend.onrender.com/ping")
+      .catch(() => {});
 
+    fetch("https://skillmutant-backend.onrender.com/warmup")
+      .catch(() => {});
+  }, []);
   // Hide chatbot when:
   // 1. User is NOT authenticated
   // 2. On room or mockinterview pages
