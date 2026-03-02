c3b9e6c8a79530872a33ed3701ad4bbb21d142ce
karthik <kandellikarthik7@gmail.com>
2026-03-02 12:08:50 +0530

    ui updating

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="relative min-h-screen w-full flex flex-col bg-[#0B1F3A] text-white">
+    <div className="relative min-h-screen w-full flex flex-col bg-[#071A2D] text-white">
 
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_at_50%_30%,rgba(255,255,255,0.05),transparent_60%)]" />
       
